39644ab155f59eb3018ff2ecdf892a464e09f7f0
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-19 17:00:25 +0000

    imports

--- client/src/components/login/userSignup/SignUpForm.jsx
+++ client/src/components/login/userSignup/SignUpForm.jsx
@@ -1,4 +1,11 @@
-import { Box, useTheme } from '@mui/material';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import InputAdornment from '@mui/material/InputAdornment';
+import { Box, useTheme, Typography } from '@mui/material';
+import Visibility from '@mui/icons-material/Visibility';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import IconButton from '@mui/material/IconButton';
 
 const SignUpForm = (props) => {
   const {
@@ -8,13 +15,15 @@ const SignUpForm = (props) => {
 
   const theme = useTheme();
   const primaryLight = theme.palette.primary.light
+  const alt = theme.palette.background.alt
+
 
   return (
     <Box
       width='100%'
       height='100%'
       textAlign='center'
-      backgroundColor={primaryLight}
+      backgroundColor={alt}
     >
       This is the full screen sign up form
       <button

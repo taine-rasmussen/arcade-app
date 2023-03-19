bd3716303a9887cefbc8e55d4399d4a67f5abaa0
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-19 13:01:44 +0000

    markup form sign up preview

--- client/src/components/login/userSignup/SignUpPreview.jsx
+++ client/src/components/login/userSignup/SignUpPreview.jsx
@@ -1,4 +1,6 @@
-import { Box, useTheme } from '@mui/material'
+import { Box, useTheme, Typography } from '@mui/material'
+import Button from '@mui/material/Button';
+
 
 const SignUpPreview = (props) => {
 
@@ -20,11 +22,25 @@ const SignUpPreview = (props) => {
       borderLeft={`7px solid ${bgLight}`}
       backgroundColor={primaryLight}
     >
-      Sign up
+      <Typography
+        fontWeight='bold'
+        fontSize='4rem'
+        color='alt'
+      >
+        New here?
+      </Typography>
+
+      <Typography
+        fontWeight='bold'
+        fontSize='1.25rem'
+        color='neutral'
+      >
+        Sign up and get right into the games
+      </Typography>
       <button
         onClick={() => setIsOnSignUpForm(!isOnSignUpForm)}
       >
-        change form
+        Sign up
       </button >
     </Box >
   )

dd92e960ff0d2a8bb9122e9864b9e2e90bacfdd1
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-19 16:02:49 +0000

    redux LOCAL

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import authReducer from './state';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

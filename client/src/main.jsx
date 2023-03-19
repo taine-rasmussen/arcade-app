365910ab90baa79333d50b65c7cbecc7e309e9a7
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-19 16:03:50 +0000

    bubble sort

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,23 @@
+import { PersistGate } from 'redux-persist/integration/react';
+import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import authReducer from './state';
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import authReducer from './state';
-import { configureStore } from '@reduxjs/toolkit';
-import { Provider } from 'react-redux';
+import {
+  persistReducer,
+  persistStore,
+  REHYDRATE,
+  REGISTER,
+  PERSIST,
+  FLUSH,
+  PAUSE,
+  PURGE
+} from 'redux-persist';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

fe9b88045a910d3c107795d80cb72a5161e6cf4b
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-21 17:37:13 +0000

    find by username

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -32,11 +32,11 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
   try {
     const {
-      email,
+      username,
       password
     } = req.body;
 
-    const user = await User.findOne({ email: email });
+    const user = await User.findOne({ username: username });
     if (!user) return res.status(400).json({ msg: 'User not found' })
 
     const isMatch = await bcrypt.compare(password, user.password);

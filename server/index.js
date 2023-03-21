4b82c4225771913fe6f185041148520d9be37d0d
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-21 14:45:45 +0000

    remove file things

--- server/index.js
+++ server/index.js
@@ -22,20 +22,6 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-
-// File storage config
-// https://github.com/expressjs/multer
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "public/assets");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname)
-  }
-});
-
-const upload = multer({ storage })
 
 // Mongoose config
 const PORT = process.env.PORT || 6001

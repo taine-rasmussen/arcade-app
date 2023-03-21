9cfb4adce3a05d45a84a2c1901a3b61558ff6e10
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-21 14:08:00 +0000

    file storage

--- server/index.js
+++ server/index.js
@@ -23,3 +23,16 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+// File storage config
+// https://github.com/expressjs/multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+});
+
+const upload = multer({ storage })

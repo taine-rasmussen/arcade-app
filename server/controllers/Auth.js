63d7f3d08d74d39325e570be36ee743cad0c1f12
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-21 14:52:52 +0000

    register user

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -1,3 +1,29 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import User from '../Models/User.js'
+
+
+// Register User
+export const register = async (req, res) => {
+  try {
+    const {
+      username,
+      password,
+      stats
+    } = req.body;
+
+    const salt = await bcrypt.genSalt();
+    const passwordhash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      password: passwordhash,
+      stats
+    });
+
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser)
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+};

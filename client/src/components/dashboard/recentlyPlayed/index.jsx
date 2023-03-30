d418bf8110afd8a227dc3b901e59c529a40656ff
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-30 22:30:19 +0100

    recentlyplayed files

---
+++ client/src/components/dashboard/recentlyPlayed/index.jsx
@@ -0,0 +1,14 @@
+import { Box, useTheme } from '@mui/material';
+import WidgetWrapper from '../../../wrapper/WidgetWrapper'
+
+const RecentlyPlayed = () => {
+  return (
+    <WidgetWrapper>
+      <Box>
+        Recently played
+      </Box>
+    </WidgetWrapper>
+  )
+}
+
+export default RecentlyPlayed

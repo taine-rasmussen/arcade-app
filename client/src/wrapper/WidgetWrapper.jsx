916900fb188a7a9fd0bc1c2278958ce2396b2c84
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-30 22:35:39 +0100

    .

--- client/src/wrapper/WidgetWrapper.jsx
+++ client/src/wrapper/WidgetWrapper.jsx
@@ -1,11 +1,11 @@
 import { Box } from '@mui/material';
 import { styled } from "@mui/system"
 
-const WidgetWrapper = styled(Box)(({ theme, width, height }) => ({
+const WidgetWrapper = styled(Box)(({ theme, width }) => ({
   padding: '1.5rem',
   width: `${width}%`,
   borderRadius: '40px',
-  height: `${height}%`,
+  height: '100%',
   minWidth: 'max-content',
   borderRadius: '0.75rem',
   padding: '1.5rem 1.5rem 0.75rem 1.5rem',

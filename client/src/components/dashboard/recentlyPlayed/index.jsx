1659cf58c8dd782eee2241513d2cce74989fe0e1
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-30 22:30:53 +0100

    set up

--- client/src/components/dashboard/recentlyPlayed/index.jsx
+++ client/src/components/dashboard/recentlyPlayed/index.jsx
@@ -2,8 +2,11 @@ import { Box, useTheme } from '@mui/material';
 import WidgetWrapper from '../../../wrapper/WidgetWrapper'
 
 const RecentlyPlayed = () => {
+
+  const theme = useTheme();
+
   return (
-    <WidgetWrapper>
+    <WidgetWrapper theme={theme} height={40} width={75}>
       <Box>
         Recently played
       </Box>

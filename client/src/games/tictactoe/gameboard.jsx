7826699415c91d3c547ee32ecbed1ba8c0d20b3c
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-04-07 21:57:11 +0100

    init game

--- client/src/games/tictactoe/gameboard.jsx
+++ client/src/games/tictactoe/gameboard.jsx
@@ -1,12 +1,52 @@
-import { Box, useTheme, useMediaQuery } from '@mui/material';
+import { useState } from 'react';
 import WidgetWrapper from '../../wrapper/WidgetWrapper';
+import { Box, useTheme, useMediaQuery } from '@mui/material';
 
+const INITGAME = [
+  {
+    id: 0,
+    value: '',
+  },
+  {
+    id: 1,
+    value: '',
+  },
+  {
+    id: 2,
+    value: '',
+  },
+  {
+    id: 3,
+    value: '',
+  },
+  {
+    id: 4,
+    value: '',
+  },
+  {
+    id: 5,
+    value: '',
+  },
+  {
+    id: 6,
+    value: '',
+  },
+  {
+    id: 7,
+    value: '',
+  },
+  {
+    id: 8,
+    value: '',
+  },
+]
 
 const Gameboard = () => {
 
   const theme = useTheme();
   const isNonMobileScreens = useMediaQuery('(min-width:1000px)');
 
+  const [game, setGame] = useState(INITGAME)
 
   return (
     <WidgetWrapper
@@ -14,7 +54,14 @@ const Gameboard = () => {
       height={75}
       theme={theme}
     >
-      TIC TAC TOE
+      <Box
+        sx={{
+          height: '100%',
+          width: '100%'
+        }}
+      >
+
+      </Box>
     </WidgetWrapper>
   )
 }

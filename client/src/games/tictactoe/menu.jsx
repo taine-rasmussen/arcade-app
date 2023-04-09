c5e006c44d1b77fb90bab01852bb01a93ea372f9
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-04-09 18:14:23 +0100

    asdfasd" git push

--- client/src/games/tictactoe/menu.jsx
+++ client/src/games/tictactoe/menu.jsx
@@ -2,8 +2,10 @@ import { Box, useTheme, useMediaQuery, Typography } from '@mui/material';
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import PeopleOutlineIcon from '@mui/icons-material/PeopleOutline';
 import WidgetWrapper from '../../wrapper/WidgetWrapper';
-import Divider from '@mui/material/Divider';
+import EditIcon from '@mui/icons-material/Edit';
 import { OutlinedInput } from '@mui/material';
+import Divider from '@mui/material/Divider';
+import { useState } from 'react';
 
 const Menu = (props) => {
 
@@ -15,6 +17,9 @@ const Menu = (props) => {
     isSinglePlayerMode,
     setIsSinglePlayerMode,
   } = props;
+
+  const [toggleNameEdit, setToggleNameEdit] = useState(false)
+  const [newName, setNewName] = useState('')
 
   const theme = useTheme();
   const isNonMobileScreens = useMediaQuery('(min-width:1000px)');
@@ -72,18 +77,28 @@ const Menu = (props) => {
           </Typography>
           <Box
             sx={{
+              gap: '2rem',
               height: '100%',
               display: 'flex',
               flexDirection: 'column',
+              justifyContent: 'center',
             }}
           >
-            {players.map((player) => (
+            <Typography variant="h4">
+              {players[0].name}
+            </Typography>
+
+            {toggleNameEdit ? (
               <OutlinedInput
-                key={player.name}
-                placeholder={player.name}
+                placeholder='Enter new name'
               />
-            )
+            ) : (
+              <Typography variant="h4" sx={{ display: 'flex', gap: '1rem' }}>
+                {players[1].name}
+                <EditIcon onClick={() => { setToggleNameEdit(!toggleNameEdit) }} />
+              </Typography>
             )}
+
           </Box>
         </Box>
         <Divider />

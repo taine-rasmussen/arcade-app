ea0d89892e31981dc2462a892c120c1aa690b680
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-04-11 22:38:53 +0100

    asdf

--- client/src/games/tictactoe/menu/ProfileCard.jsx
+++ client/src/games/tictactoe/menu/ProfileCard.jsx
@@ -1,7 +1,8 @@
 import { Box, useTheme, useMediaQuery, Typography } from '@mui/material';
+import TripOriginIcon from '@mui/icons-material/TripOrigin';
 import WidgetWrapper from '../../../wrapper/WidgetWrapper'
+import CloseIcon from '@mui/icons-material/Close';
 import { useSelector } from 'react-redux';
-
 
 
 const ProfileCard = (props) => {
@@ -15,18 +16,30 @@ const ProfileCard = (props) => {
 
   const theme = useTheme()
   const main = theme.palette.background.main
+  const borderColor = theme.palette.primary.main
   const loggedInUsername = useSelector((state) => state.user.username)
   const isLoggedInUser = loggedInUsername === name;
 
+  const iconStlye = {
+    fontSize: '2rem'
+  }
+
   return (
-    <WidgetWrapper theme={theme} height={40} width={45}>
+    <WidgetWrapper
+      theme={theme}
+      height={40}
+      width={45}
+      sx={
+        isLoggedInUser ? { border: `4px solid ${borderColor}` } : null
+      }
+    >
       <Box
         sx={{
           gap: '1rem',
           display: 'flex',
           alignItems: 'center',
           flexDirection: 'column',
-          justifyContent: 'center',
+          justifyContent: 'spaceBetween',
         }}
       >
         <Box
@@ -42,12 +55,14 @@ const ProfileCard = (props) => {
         >
           Pic
         </Box>
-        <Typography
-          variant='h2'
-        >
+        <Typography variant='h2'>
           {name}
         </Typography>
-        <Typography></Typography>
+        <Typography variant='h3'>
+          {isLoggedInUser
+            ? <TripOriginIcon sx={iconStlye} />
+            : <CloseIcon sx={iconStlye} />}
+        </Typography>
 
 
       </Box>

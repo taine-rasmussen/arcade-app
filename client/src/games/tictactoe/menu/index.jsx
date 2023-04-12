9a27c942eae620b3a36982393a0ae163fb125db9
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-04-12 21:40:12 +0100

    tidy

--- client/src/games/tictactoe/menu/index.jsx
+++ client/src/games/tictactoe/menu/index.jsx
@@ -1,6 +1,5 @@
-import { Box, useTheme, useMediaQuery, Typography } from '@mui/material';
 import FlexBetween from '../../../wrapper/FlexBetween'
-import { useState } from 'react';
+import { Box, useMediaQuery } from '@mui/material';
 
 import ProfileCard from './ProfileCard';
 import Settings from './Settings';
@@ -9,26 +8,11 @@ const Menu = (props) => {
 
   const {
     players,
-    resetGame,
     playerTurn,
     setPlayers,
-    isSinglePlayerMode,
-    setIsSinglePlayerMode,
   } = props;
 
-  const [toggleNameEdit, setToggleNameEdit] = useState(false)
-  const [newName, setNewName] = useState('')
-  const [newNamePlaceholder, setNewNamePlaceholder] = useState('Enter new name...')
-
-  const theme = useTheme();
   const isNonMobileScreens = useMediaQuery('(min-width:1000px)');
-
-  const updatePlayerTwoName = () => {
-    if (newName.length < 1) return setNewNamePlaceholder('Enter a name first');
-    setPlayers([...players], players[1].name = newName)
-    setNewName('')
-    setToggleNameEdit(!toggleNameEdit)
-  }
 
   return (
     <Box
@@ -51,42 +35,6 @@ const Menu = (props) => {
         ))}
       </FlexBetween>
       <Settings />
-
-      {/* 
-        <NavHeader resetGame={resetGame} />
-        <Divider />
-        <FlexBetween sx={wrapperMargin} >
-          <Typography variant="h3">
-            Gamemode
-          </Typography>
-          <Box
-            sx={{ cursor: 'pointer' }}
-            onClick={() => { setIsSinglePlayerMode(!isSinglePlayerMode) }}
-          >
-            {isSinglePlayerMode ? <PersonOutlineIcon sx={iconTheme} /> : <PeopleOutlineIcon sx={iconTheme} />}
-          </Box>
-        </FlexBetween>
-        <Divider />
-        <FlexBetween sx={wrapperMargin} >
-          <Typography variant="h3">
-            {players[0].name}
-          </Typography>
-
-          {toggleNameEdit ? (
-            <OutlinedInput
-              value={newName}
-              placeholder={newNamePlaceholder}
-              onChange={(e) => { setNewName(e.target.value) }}
-              endAdornment={<EditIcon onClick={updatePlayerTwoName} />}
-            />
-          ) : (
-            <Typography variant="h3" sx={{ display: 'flex', gap: '1rem' }}>
-              {players[1].name}
-              <EditIcon onClick={() => { setToggleNameEdit(!toggleNameEdit) }} />
-            </Typography>
-          )}
-        </FlexBetween>
-        <Divider /> */}
     </Box>
   )
 }

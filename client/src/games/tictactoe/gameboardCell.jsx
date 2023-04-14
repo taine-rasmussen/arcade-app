f6a0c6709c31cffe19bf155b078546b43086b396
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-04-14 18:30:16 +0100

    aeasdf

--- client/src/games/tictactoe/gameboardCell.jsx
+++ client/src/games/tictactoe/gameboardCell.jsx
@@ -32,32 +32,6 @@ const GameboardCell = (props) => {
     playerTurn === 0 ? setPlayerTurn(1) : setPlayerTurn(0)
   };
 
-  const checkForWin = () => {
-    const winningCombos = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-
-    for (let i = 0; i < winningCombos.length; i++) {
-      const [a, b, c] = winningCombos[i];
-      const cellA = game.find(cell => cell.id === a);
-      const cellB = game.find(cell => cell.id === b);
-      const cellC = game.find(cell => cell.id === c);
-
-      if (cellA.value && cellA.value === cellB.value && cellB.value === cellC.value) {
-        setIsGameOver(true)
-        setCurrentWinner(players[playerTurn])
-      }
-    }
-    return null;
-  }
-
   return (
     <Box
       key={id}
--- client/src/games/tictactoe/useTicTacToeApi.js
+++ client/src/games/tictactoe/useTicTacToeApi.js
@@ -13,13 +13,44 @@ const useTicTacToeApi = () => {
   const [isSinglePlayerMode, setIsSinglePlayerMode] = useState(false);
   const [players, setPlayers] = useState([{ name: loggedInUsername }, { name: 'playerTwo' }])
 
+  const checkForWin = () => {
+    const winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let i = 0; i < winningCombos.length; i++) {
+      const [a, b, c] = winningCombos[i];
+      const cellA = game.find(cell => cell.id === a);
+      const cellB = game.find(cell => cell.id === b);
+      const cellC = game.find(cell => cell.id === c);
+
+      if (cellA.value && cellA.value === cellB.value && cellB.value === cellC.value) {
+        setIsGameOver(true)
+        setCurrentWinner(players[playerTurn])
+      }
+    }
+    return null;
+  }
+
 
   const state = {
-
+    game,
+    players,
+    playerTurn,
+    isGameOver,
+    currentWinner,
+    isSinglePlayerMode,
   };
 
   const funcs = {
-
+    checkForWin
   };
 
   return { state, funcs }

2dc00b377db5e60fcd094b1c3afc07a6055a9021
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-04-17 23:50:57 +0100

    asdf

--- client/src/games/tictactoe/index.jsx
+++ client/src/games/tictactoe/index.jsx
@@ -44,6 +44,16 @@ const INITGAME = [
     value: '',
   },
 ]
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
 
 export const GameContext = createContext();
 
@@ -78,16 +88,6 @@ const TicTacToe = () => {
         playerTurn: !state.playerTurn
       }
     } else if (action === 'checkWin') {
-      const winningCombos = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-      ];
       for (let i = 0; i < winningCombos.length; i++) {
         const [a, b, c] = winningCombos[i];
         const cellA = state.game.find(cell => cell.id === a);

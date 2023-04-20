257a108d6ce503408c7b938eba9b7ba96abdb8d5
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-04-20 22:00:48 +0100

    asdf

--- client/src/games/tictactoe/menu/EditNameWidget.jsx
+++ client/src/games/tictactoe/menu/EditNameWidget.jsx
@@ -3,10 +3,25 @@ import { GameContext } from '../index';
 import { Box, useTheme, Typography } from '@mui/material';
 
 const EditNameWidget = () => {
+  const [isEdit, setIsEdit] = useState(false);
+  const { dispatch, state: { isSinglePlayerMode } } = useContext(GameContext);
+
+
+
+
+
   return (
-    <div>
-      Edit~!
-    </div>
+    <Box>
+      <Typography
+        variant='h2'
+      >
+        {
+          isSinglePlayerMode
+            ? 'Single player'
+            : 'Two player'
+        }
+      </Typography>
+    </Box>
   )
 }
 

64531abd1e42a905f7a96056e834cb5cb29ba9d6
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-04-20 22:15:16 +0100

    asdf

--- client/src/games/tictactoe/menu/EditGameModeWidget.jsx
+++ client/src/games/tictactoe/menu/EditGameModeWidget.jsx
@@ -1,6 +1,12 @@
+import { Box, useTheme, Typography } from '@mui/material';
+import InputAdornment from '@mui/material/InputAdornment';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import IconButton from '@mui/material/IconButton';
+import SendIcon from '@mui/icons-material/Send';
 import { useState, useContext } from 'react';
 import { GameContext } from '../index';
-import { Box, useTheme, Typography } from '@mui/material';
+
 
 const Display = () => {
   const { state: { isSinglePlayerMode } } = useContext(GameContext);
@@ -19,14 +25,52 @@ const Display = () => {
   )
 }
 
+const Input = ({ isEdit, setIsEdit }) => {
+  const theme = useTheme();
+  const [newName, setNewName] = useState('')
+  const bgLight = theme.palette.neutral.light
+
+  return (
+    <>
+      <InputLabel >Enter name</InputLabel>
+      <OutlinedInput
+        onChange={(e) => setNewName(e.target.value)}
+        sx={{ background: bgLight, fontSize: '1.1rem' }}
+        value={newName}
+        inputProps={{
+          'aria-label': 'weight',
+        }}
+        label="Enter name..."
+        endAdornment={
+          <InputAdornment position="end">
+            <IconButton
+              aria-label="toggle password visibility"
+              onClick={handleClickShowPassword}
+              onMouseDown={handleMouseDownPassword}
+              edge="end"
+            >
+              {showPassword ? <VisibilityOff /> : <Visibility />}
+            </IconButton>
+      />
+    </>
+  )
+}
+
 const EditGameModeWidget = () => {
   const [isEdit, setIsEdit] = useState(false);
   return (
-    <>
-      {isEdit
-        ? <Input />
-        : <Display />}
-    </>
+      <>
+        {isEdit
+          ? <Input
+            setIsEdit={setIsEdit}
+            isEdit={isEdit}
+          />
+          : <Display
+            setIsEdit={setIsEdit}
+            isEdit={isEdit}
+          />
+        }
+      </>
   )
 }
 

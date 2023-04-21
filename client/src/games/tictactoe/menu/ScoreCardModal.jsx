0796f63a22156113d258f01f395cedd30c55483f
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-04-21 11:50:22 +0100

    tidy up

--- client/src/games/tictactoe/menu/ScoreCardModal.jsx
+++ client/src/games/tictactoe/menu/ScoreCardModal.jsx
@@ -18,17 +18,17 @@ const ScoreCardModal = ({ openModal, setOpenModal, result }) => {
     >
       <Box
         sx={{
-          border: '2px solid #000',
-          position: 'absolute',
-          height: '70%',
-          width: '50%',
           top: '50%',
           left: '50%',
-          transform: 'translate(-50%, -50%)',
+          width: '50%',
+          height: '70%',
           display: 'grid',
           gridGap: '15px',
+          position: 'absolute',
+          border: '2px solid #000',
           backgroundColor: 'transparent',
           gridTemplateRows: 'repeat(3, 1fr)',
+          transform: 'translate(-50%, -50%)',
           gridTemplateColumns: 'repeat(3, 1fr)',
         }}
       >

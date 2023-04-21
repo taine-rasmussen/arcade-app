c71e9e4ae2b3ad2398cf4b40ff952cf71ca08aab
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-04-21 11:29:13 +0100

    asdf

--- client/src/games/tictactoe/menu/ScoreCard.jsx
+++ client/src/games/tictactoe/menu/ScoreCard.jsx
@@ -1,34 +1,70 @@
-import { useTheme, Typography } from '@mui/material';
+import { useTheme, Typography, Box } from '@mui/material';
+import Modal from '@mui/material/Modal';
 import { motion } from 'framer-motion';
+import { useState } from 'react'
 
 const ScoreCard = (result) => {
+
+  const [openModal, setOpenModal] = useState(false)
+
   const theme = useTheme()
   const bg = theme.palette.background.main
+
+
+  const modalStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
   return (
-    <motion.div
-      animate={{ x: ['150px', '0px'] }}
-      transition={{
-        type: "spring",
-        stiffness: 260,
-        damping: 20
-      }}
-      style={{
-        width: '90%',
-        background: bg,
-        display: 'flex',
-        minHeight: '20%',
-        alignItems: 'center',
-        borderRadius: '10px',
-        justifyContent: 'center',
-        border: '3px solid #000',
-      }}
-    >
-      <Typography
-        variant='h3'
+    <>
+      <motion.div
+        animate={{ x: ['150px', '0px'] }}
+        transition={{
+          type: "spring",
+          stiffness: 260,
+          damping: 20
+        }}
+        style={{
+          width: '90%',
+          background: bg,
+          display: 'flex',
+          minHeight: '20%',
+          alignItems: 'center',
+          borderRadius: '10px',
+          justifyContent: 'center',
+          border: '3px solid #000',
+        }}
+        className='hover'
+        onClick={() => { setOpenModal(true) }}
       >
-        {result.result ? 'X' : 'O'}
-      </Typography>
-    </motion.div>
+        <Typography
+          variant='h3'
+        >
+          {result.result ? 'X' : 'O'}
+        </Typography>
+      </motion.div>
+
+      <Modal
+        open={openModal}
+        onClose={() => setOpenModal(false)}
+        aria-labelledby="parent-modal-title"
+        aria-describedby="parent-modal-description"
+      >
+        <Box
+          sx={modalStyle}
+        >
+          'working???'
+        </Box>
+      </Modal>
+    </>
   )
 }
 

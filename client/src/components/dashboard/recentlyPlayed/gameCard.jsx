3608df84b49c06568a2f92584b020f0a97c005b1
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-04-28 21:44:21 +0100

    asdf

--- client/src/components/dashboard/recentlyPlayed/gameCard.jsx
+++ client/src/components/dashboard/recentlyPlayed/gameCard.jsx
@@ -1,20 +1,44 @@
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
 import { Box, useTheme, Typography } from '@mui/material';
 
 const GameCard = ({ game }) => {
 
   const theme = useTheme();
-  const main = theme.palette.primary.alt
+  const hightlight = theme.palette.primary.hightlight
 
   return (
     <Box
       sx={{
         height: '60%',
         width: '80%',
-        background: '#fff',
-        borderRadius: '0.75rem',
+        display: 'flex',
+        flexDirection: 'column'
       }}
     >
-      {game}
+      <Box
+        sx={{
+          width: '40px',
+          height: '40px',
+          display: 'flex',
+          padding: '15px',
+          alignItems: 'center',
+          background: hightlight,
+          borderRadius: '0.75rem',
+          justifyContent: 'center',
+        }}
+      >
+        <EmojiEventsIcon sx={{ color: '#fff' }} />
+      </Box>
+      <Box
+        sx={{
+          height: '60%',
+          width: '80%',
+          background: '#fff',
+          borderRadius: '0.75rem',
+        }}
+      >
+        {game}
+      </Box>
     </Box>
   )
 }

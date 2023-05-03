91f6e300303b85ae4a81a62f383465145c710126
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-05-03 14:17:15 +0100

    wire up all games with routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -28,8 +28,12 @@ const App = () => {
           <CssBaseline />
           <Routes>
             <Route path='/' element={<Login />} />
+            <Route path='pong' element={isAuth ? <Pong /> : <Navigate to='/' />} />
+            <Route path='snake' element={isAuth ? <Snake /> : <Navigate to='/' />} />
+            <Route path='memory' element={isAuth ? <Memory /> : <Navigate to='/' />} />
             <Route path='dashboard' element={isAuth ? <Dashboard /> : <Navigate to='/' />} />
             <Route path='tictactoe' element={isAuth ? <TicTacToe /> : <Navigate to='/' />} />
+            <Route path='Minesweeper' element={isAuth ? <Minesweeper /> : <Navigate to='/' />} />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>

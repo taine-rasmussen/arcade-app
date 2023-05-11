fc9d5056997c587b7e222dfe7f2e5dbadab79084
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-05-11 22:07:48 +0100

    tidy

--- client/src/components/dashboard/games/gamePreviewCard.jsx
+++ client/src/components/dashboard/games/gamePreviewCard.jsx
@@ -1,8 +1,6 @@
-import { setRecentlyPlayed } from '../../../state/index';
 import GamePreviewCard from './GamePreviewCard';
 import { Box, Typography } from "@mui/material"
 import { Box, useTheme } from '@mui/material';
-import { useDispatch } from 'react-redux';
 
 
 const GamePreviewCard = (props) => {

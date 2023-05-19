c43d702cf5fa7e68d39ab07ef42a7aaee3d71db1
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-05-19 22:43:06 +0100

    tidy it up a bit

--- client/src/games/tictactoe/menu/BtnGroup.jsx
+++ client/src/games/tictactoe/menu/BtnGroup.jsx
@@ -1,3 +1,4 @@
+import AccessAlarmsIcon from '@mui/icons-material/AccessAlarms';
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 import FlexBetween from '../../../wrapper/FlexBetween';
 import ButtonGroup from '@mui/material/ButtonGroup';
@@ -15,6 +16,17 @@ const BtnGroup = ({ setIsEdit, isEdit, seconds }) => {
   const { dispatch, state: { isSinglePlayerMode } } = useContext(GameContext);
 
   const btnStyle = {
+    fontSize: '1.25rem',
+    borderRadius: '0.75rem',
+    background: alt,
+    '&:hover': {
+      background: alt,
+      color: highlight
+    }
+  }
+
+  const timerBtnStyles = {
+    width: '45px',
     fontSize: '1.25rem',
     borderRadius: '0.75rem',
     background: alt,
@@ -42,24 +54,23 @@ const BtnGroup = ({ setIsEdit, isEdit, seconds }) => {
           Reset
       </Button>
         <Button
-          onClick={() => { dispatch({ type: 'toggleGameMode' }) }}
-          sx={btnStyle}
-          endIcon={
-            isSinglePlayerMode
-              ? <PersonIcon />
-              : <PeopleAltIcon />
-          }
-        >
-          Mode
-      </Button>
-        <Button
           onClick={() => { setIsEdit(!isEdit); }}
           sx={btnStyle}
         >
           Edit
       </Button>
         <Button
+          onClick={() => { dispatch({ type: 'toggleGameMode' }) }}
           sx={btnStyle}
+        >
+          {
+            isSinglePlayerMode
+              ? <PersonIcon />
+              : <PeopleAltIcon />
+          }
+        </Button>
+        <Button
+          sx={timerBtnStyles}
         >
           {seconds}
         </Button>
